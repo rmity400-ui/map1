de65556807e7e988c93fcc7f005a4d9c9ab3fd77
school-website-app-now <rmity400@gmail.com>
2026-05-24 11:23:53 +0700

    upate add firbase

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBq_1YKH4Hf4M65qMHirvWCD_-tyqCDz5E",
   authDomain: "ramit-7e364.firebaseapp.com",
   projectId: "ramit-7e364",
-  storageBucket: "ramit-7e364.firebasestorage.app",
+  storageBucket: "ramit-7e364.appspot.com",
   messagingSenderId: "1036691345731",
-  appId: "1:1036691345731:web:df8121852c6137e3b35ff6",
-  measurementId: "G-99Y1VSYHJG"
+  appId: "1:1036691345731:web:df8121852c6137e3b35ff6"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
